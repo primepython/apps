c64854dd47f4582c47bcbcefa3c48c64e22c52ac
primepython <123320577+primepython@users.noreply.github.com>
2024-05-09 13:19:51 +0200

    Update energy_check.py

--- energy_check.py
+++ energy_check.py
@@ -1,14 +1,15 @@
+#For image sample, please refer to energy_check_sample.jpg
 import os
 import pytesseract
 import cv2
-
+#Recognition function
 def image_processing():
     img = cv2.imread(filename)
     scaled_image = cv2.resize(img, None, fx=7.0, fy=7.0, interpolation=cv2.INTER_LANCZOS4)
     gray = cv2.cvtColor(scaled_image, cv2.COLOR_BGR2GRAY)
     text = pytesseract.image_to_string(gray, lang='eng')
     print(text)
-
+#Check on multiple files within directory
 directory = './'
 desired_extension = '.jpg'
 for filename in os.listdir(directory):
